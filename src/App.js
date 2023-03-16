c20cf2de7cc01b4a5c503325fd56bcc9d8b787f9
sanjib <sanjib.adhya@zacksindia.com>
2023-03-16 07:38:04 -0500

    added project files for custom cursor using framer motion with React 18

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState} from 'react'
+import {motion} from 'framer-motion'
 
-function App() {
+const App = () => {
+
+  const[mousePosition, setMousePosition] = useState({x:0, y: 0})
+  const [cursorVariant, setCursorVariant] = useState('default')
+
+  useEffect(()=>{
+    const mouseMove = e=>{
+      setMousePosition({
+        x: e.clientX,
+        y: e.clientY
+      })
+    }
+
+    window.addEventListener('mousemove', mouseMove)
+
+    return ()=>{
+      window.addEventListener('mousemove', mouseMove)
+    }
+  }, [])
+
+  const variants = {
+    default:{
+      x: mousePosition.x - 16,
+      y: mousePosition.y - 16,
+    },
+    text:{
+      height: 150,
+      width: 150,
+      x: mousePosition.x - 75,
+      y: mousePosition.y - 75,
+      backgroundColor: 'yellow',
+      mixBlendMode: 'difference'
+    }
+  }
+
+  const textEnter = () => setCursorVariant('text')
+  const textLeave = () => setCursorVariant('default')
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <h1 onMouseEnter={textEnter} onMouseLeave={textLeave} className='title'>Hello world</h1>
+      <motion.div 
+        className='cursor'
+        variants={variants}
+        animate={cursorVariant}
+      />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
